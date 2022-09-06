5bad06a9b574d8c2746ceb7e9ecaafd8b9f80c46
ericchee08 <58150120+ericchee08@users.noreply.github.com>
2022-09-06 22:30:25 +0100

    routing added, sign-in component created, angular materials installed and bootstrap installed

---
+++ src/app/components/sign-in-page/sign-in-page.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignInPageComponent } from './sign-in-page.component';
+
+@NgModule({
+  declarations: [SignInPageComponent],
+  imports: [CommonModule],
+  exports: [SignInPageComponent],
+})
+export class SignInPageModule {}
