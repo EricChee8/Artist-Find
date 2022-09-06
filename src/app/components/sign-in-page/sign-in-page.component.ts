ee638705afbc8696f77f2df06cdb891b5b2f7473
ericchee08 <110496410+EricChee8@users.noreply.github.com>
2022-09-06 23:30:17 +0100

    bootstrap implemented

--- src/app/components/sign-in-page/sign-in-page.component.ts
+++ src/app/components/sign-in-page/sign-in-page.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-in-page',
   templateUrl: './sign-in-page.component.html',
-  styleUrls: ['./sign-in-page.component.scss']
+  styleUrls: ['./sign-in-page.component.scss'],
 })
 export class SignInPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
